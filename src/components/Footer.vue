046d219e7db010f337708581cacd59938fdedced
Zach McDonald-Wright <A1064877@nzdf.mil.nz>
2024-03-22 09:54:59 +1300

    make boat

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Red Hat OpenShift (ARO) workshop
+        Developed by Team Sistema
       </div>
       <div class="col-lg-6">
       </div>
